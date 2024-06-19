f0b522df35f5f8c2cab5abd7d592769bab74d766
Shivansh-22866 <shivanshp0418@gmail.com>
2024-06-19 23:34:36 +0530

    API add and delete completed

--- frontend/lib/providers/notes_provider.dart
+++ frontend/lib/providers/notes_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/models/note.dart';
+import 'package:frontend/services/api_service.dart';
 
 class NotesProvider with ChangeNotifier {
   List<Note> notes = [];
@@ -13,11 +14,13 @@ class NotesProvider with ChangeNotifier {
   void addNote(Note note) {
     notes.add(note);
     notifyListeners();
+    ApiService.addNote(note);
   }
 
   void deleteNote(Note note) {
     int index = notes.indexOf(notes.firstWhere((ele) => ele.id == note.id));
     notes.removeAt(index);
     notifyListeners();
+    ApiService.deleteNote(note);
   }
 }
--- frontend/lib/services/api_service.dart
+++ frontend/lib/services/api_service.dart
@@ -1,3 +1,23 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:frontend/models/note.dart';
+import 'package:http/http.dart' as http;
+
 class ApiService {
-  static String _baseUrl = "https://flutter-node-1.onrender.com/";
+  static String _baseUrl = "https://flutter-node-1.onrender.com/notes";
+
+  static Future<void> addNote(Note note) async {
+    Uri requestUri = Uri.parse("$_baseUrl/add");
+    var res = await http.post(requestUri, body: note.toMap());
+    var decoded = jsonDecode(res.body);
+    log(decoded.toString());
+  }
+
+  static Future<void> deleteNote(Note note) async {
+    Uri requestUri = Uri.parse("$_baseUrl/delete");
+    var res = await http.delete(requestUri, body: note.toMap());
+    var decoded = jsonDecode(res.body);
+    log(decoded.toString());
+  }
 }
